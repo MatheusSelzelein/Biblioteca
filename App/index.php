c7d873162d22a82601dcd9158b82e895070aa2ec
Matheus Selzelein <matheusselzelein@gmail.com>
2026-08-17 22:51:01 -0300

    Correção

--- App/index.php
+++ App/index.php
@@ -1,12 +1,12 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 session_start();
 
 // faz o include dos arquivos
-require_once __DIR__ . "/config.php";
-require_once __DIR__ . "/autoload.php";
-require_once __DIR__ . "/routes.php";
+include "config.php";
+include "autoload.php";
+include "routes.php";
+    
     
 
